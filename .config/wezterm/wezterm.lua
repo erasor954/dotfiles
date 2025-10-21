cfcfdd532fd82392c009327bfdd4ec26054cd881
Alex Rull <alex.rull0809@gmail.com>
2025-10-21 19:07:10 +0200

    remove nvim config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,92 +11,24 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- redefine brewer colors to better contrast
-local mybrewerlight = wezterm.color.get_builtin_schemes()['Brewer (light) (terminal.sexy)']
-mybrewerlight.background = 'white'
-mybrewerlight.foreground = 'black'
-
-local mybrewerdark = wezterm.color.get_builtin_schemes()['Brewer (dark) (terminal.sexy)']
-mybrewerdark.foreground = 'white'
-mybrewerdark.background = 'black'
-
-window_background_opacity = 0.9
 
 config.color_schemes = {
   ['mybrewer light'] = mybrewerlight,
   ['mybrewer dark'] = mybrewerdark,
 }
 
-local lighttheme = 'mybrewer light'
-local darktheme =  'mybrewer dark'
+-- Catppuccin Latte
+local lighttheme = 'Catppuccin Latte'
 
+-- Catppuccin Mocha
+-- Tokyo Night
+local darktheme = 'Ayu Dark (Gogh)'
 
-
--- This is where you actually apply your config choices
--- local lighttheme = 'zenbones_light'
--- local darktheme =  'zenbones_dark'
--- local lighttheme = 'zenwritten_light'
--- local darktheme =  'zenwritten_dark'
--- local lighttheme = 'Brewer (light) (terminal.sexy)'
--- local darktheme =  'Brewer (dark) (terminal.sexy)'
--- local lighttheme = 'Papercolor Light (Gogh)'
--- local darktheme = 'Papercolor Dark (Gogh)'
---
--- local darktheme = 'Humanoid dark (base16)'
--- local lighttheme ='Humanoid light (base16)'
---
--- local darktheme = 'Ef-Deuteranopia-Dark'
--- local lighttheme = 'Ef-Deuteranopia-Light'
---
--- local lighttheme = 'Ef-Duo-Light'
--- local darktheme = 'Ef-Duo-Dark'
---
--- local lighttheme = 'Ef-Maris-Light'
--- local darktheme = 'Ef-Maris-Dark'
---
--- local lighttheme = 'Ef-Trio-Light'
--- local darktheme = 'Ef-Trio-Dark'
---
--- local lighttheme ='flexoki-light'
--- local darktheme ='flexoki-dark'
-
--- local lighttheme = 'Atelierheath (light) (terminal.sexy)'
--- local darktheme =  'Atelierheath (dark) (terminal.sexy)'
-
--- local lighttheme = 'Atelierforest (light) (terminal.sexy)'
--- local darktheme =  'Atelierforest (dark) (terminal.sexy)'
-
--- local lighttheme = 'Atelierforest (light) (terminal.sexy)'
--- local darktheme =  'Atelierforest (dark) (terminal.sexy)'
-
--- local lighttheme = 'Ayu Light (Gogh)'
--- local darktheme =  'Ayu Dark (Gogh)'
-
--- local lighttheme = 'Google (light) (terminal.sexy)'
--- local darktheme =  'Google (dark) (terminal.sexy)'
--- 
--- zu grell:
--- local lighttheme ='Builtin Dark'
--- local darktheme = 'Builtin Light'
--- zu verspielt
--- local lighttheme ='Builtin Solarized Dark'
--- local darktheme = 'Builtin Solarized Light'
--- 'Atelierforest (light) (terminal.sexy)'
--- "Alabaster"
--- "Adventure"
-
-
--- set on startup
-
--- redefine background color in Brewer light theme to white
-
-
+config.window_background_opacity = 1
 config.color_scheme = darktheme
--- config.foreground_text_hsb = {hue = 1.0,saturation = 0.9,brightness = 1.0}
 
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 18
---config.force_reverse_video_cursor = true
 
 local basefont = 'JetBrains Mono'
 local regularfont = wezterm.font(basefont, { weight = 'Regular'})
@@ -111,30 +43,11 @@ if (config.notification_handling ~= nil) then
   config.notification_handling = 'AlwaysShow'
 end
 
--- tilde on macos
-config.use_dead_keys = true
-
-
 -- os specifics
--- windows
-if string.find(wezterm.target_triple, 'windows') ~= nil then
-  myenv="windows"
-  front_end="Software"
-  winenv=os.getenv("HOMEDRIVE")..os.getenv("HOMEPATH")
-  config.default_prog = { winenv .. '\\scoop\\apps\\git\\current\\bin\\bash.exe', '-l' }
-end
 -- linux
 if string.find(wezterm.target_triple, 'linux') ~= nil then
   myenv="linux"
 end
--- macos
-if string.find(wezterm.target_triple, 'apple') ~= nil then
-  myenv="apple"
-  config.window_decorations = "TITLE|RESIZE|MACOS_FORCE_DISABLE_SHADOW"
-  config.send_composed_key_when_left_alt_is_pressed = true
-  --  config.front_end = "WebGpu"
-end
-
 
 
 -- function on event toggle-theme - see config.keys
@@ -148,28 +61,7 @@ wezterm.on('toggle-theme', function(window, pane)
   window:set_config_overrides(overrides)
 end)
 
--- function on event toggle-fontweight - see config.keys
-wezterm.on('toggle-fontweight', function(window, pane)
-  local fullconfig = window:effective_config()
-  local overrides = window:get_config_overrides() or {}
-  if fullconfig.font.font[1].weight == "Regular" then
-    overrides.font = boldfont
-  else
-    overrides.font = regularfont
-  end
-  window:set_config_overrides(overrides)
-end)
-
-
-
 config.keys = {
-  {
-    -- Turn off non-breaking spaces in wezterm on MacOS
-    key = ' ',
-    mods = 'OPT',
-    action = wezterm.action.SendKey { key = ' '},
-  },
-
   {
     key = 'w',
     mods = 'SHIFT|CTRL',
@@ -196,15 +88,6 @@ config.keys = {
     mods = 'CTRL',
     action = wezterm.action.EmitEvent 'toggle-theme',
   },
-  {
-    key = '4',
-    mods = 'CTRL',
-    action = wezterm.action.EmitEvent 'toggle-fontweight',
-  },
-  {
-    key = 'F11',
-    action = wezterm.action.ToggleFullScreen,
-  },
 }
 
 local mux = wezterm.mux
@@ -215,7 +98,6 @@ wezterm.on('gui-startup', function(window)
   gui_window:maximize()
 end)
 
-config.window_background_opacity = 0.8 
 
 config.window_padding = {
     left=0,
@@ -227,6 +109,7 @@ config.window_decorations = "NONE"
 
 config.use_resize_increments = false
 config.adjust_window_size_when_changing_font_size = false
+
 -- and finally, return the configuration to wezterm
 return config
 
