d8605dd3bdd117613f249d2083795a3f11e36de9
Alex Rull <erasor@erasors-MacBook.local>
2026-07-02 21:53:59 +0200

    update

--- .config/nvim/lua/erasor/plugins/treesitter.lua
+++ .config/nvim/lua/erasor/plugins/treesitter.lua
@@ -2,5 +2,15 @@
 -- vim.pack.add({ gh("romus204/tree-sitter-manager.nvim") })
 
 -- require("tree-sitter-manager").setup({})
-vim.pack.add({gh("arborist-ts/arborist.nvim")})
-require("arborist").setup()
+vim.pack.add({ gh("arborist-ts/arborist.nvim") })
+require("arborist").setup({
+	install_popular = false,
+	ensure_installed = {
+		"lua",
+		"javascript",
+		"typescript",
+		"html",
+		"css",
+		"java",
+	},
+})
