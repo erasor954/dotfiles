6273c7218a077d324f6a3cc81aead38d6c5d842b
Alex Rull <erasor@erasors-MacBook.local>
2026-07-07 22:00:00 +0200

    tabswtich from leader + number to cmd + number

--- .config/wezterm/config/bindings.lua
+++ .config/wezterm/config/bindings.lua
@@ -75,15 +75,15 @@ local keys = {
 	{ key = "]", mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
 
 	-- tabs: switch by number
-	{ key = "1", mods = "LEADER", action = act.ActivateTab(0) },
-	{ key = "2", mods = "LEADER", action = act.ActivateTab(1) },
-	{ key = "3", mods = "LEADER", action = act.ActivateTab(2) },
-	{ key = "4", mods = "LEADER", action = act.ActivateTab(3) },
-	{ key = "5", mods = "LEADER", action = act.ActivateTab(4) },
-	{ key = "6", mods = "LEADER", action = act.ActivateTab(5) },
-	{ key = "7", mods = "LEADER", action = act.ActivateTab(6) },
-	{ key = "8", mods = "LEADER", action = act.ActivateTab(7) },
-	{ key = "9", mods = "LEADER", action = act.ActivateTab(8) },
+	-- { key = "1", mods = "LEADER", action = act.ActivateTab(0) },
+	-- { key = "2", mods = "LEADER", action = act.ActivateTab(1) },
+	-- { key = "3", mods = "LEADER", action = act.ActivateTab(2) },
+	-- { key = "4", mods = "LEADER", action = act.ActivateTab(3) },
+	-- { key = "5", mods = "LEADER", action = act.ActivateTab(4) },
+	-- { key = "6", mods = "LEADER", action = act.ActivateTab(5) },
+	-- { key = "7", mods = "LEADER", action = act.ActivateTab(6) },
+	-- { key = "8", mods = "LEADER", action = act.ActivateTab(7) },
+	-- { key = "9", mods = "LEADER", action = act.ActivateTab(8) },
 
 	-- tab: title
 	{ key = "0", mods = mod.SUPER, action = act.EmitEvent("tabs.manual-update-tab-title") },
@@ -217,6 +217,14 @@ local mouse_bindings = {
 		action = act.OpenLinkAtMouseCursor,
 	},
 }
+
+for i = 1, 9 do
+	table.insert(keys, {
+		key = tostring(i),
+		mods = mod.SUPER,
+		action = act.ActivateTab(i - 1),
+	})
+end
 
 return {
 	disable_default_key_bindings = true,
