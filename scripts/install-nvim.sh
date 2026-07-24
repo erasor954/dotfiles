cbd7be777dc83b49cb8dc2f005042a4b41f93d3b
Alex Rull <erasor@erasors-MacBook.local>
2026-07-24 22:19:34 +0200

    fixed an error

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -7,4 +7,5 @@ tar -xzf nvim-linux-x86_64.tar.gz
 mv nvim-linux-x86_64 "$HOME/.local/share/nvim"
 ln -sf "$HOME/.local/share/nvim/bin/nvim" "$HOME/.local/bin/nvim"
 rm -rf nvim-linux-x86_64.tar.gz
+cd "$HOME/dotfiles" || exit 1
 bash "$HOME/dotfiles/scripts/update-nvim-config.sh"
