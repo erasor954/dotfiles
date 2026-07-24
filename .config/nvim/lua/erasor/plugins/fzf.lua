2c459c5698201c2d3547d3d91a408353a38d1f18
Alex Rull <erasor@erasors-MacBook.local>
2026-07-24 21:53:11 +0200

    update

--- .config/nvim/lua/erasor/plugins/fzf.lua
+++ .config/nvim/lua/erasor/plugins/fzf.lua
@@ -11,6 +11,7 @@ require("fzf-lua").setup({
 })
 
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "[F]zfLua [F]iles" })
+vim.keymap.set("n", "<leader>fa", "<cmd>FzfLua git_files<cr>", { desc = "[F]zfLua [A]ll files in git repo" })
 vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "[F]zfLua [B]uffers" })
 vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "[F]zfLua [G]rep" })
 vim.keymap.set("n", "<leader>ca", "<cmd>FzfLua lsp_code_actions<cr>", { desc = "[C]ode [A]ctions" })
