23d9b97cda1bd581ccd5cd6558b4f46e5cbabd50
Alex Rull <erasor@erasors-MacBook.local>
2026-07-24 22:14:52 +0200

    fixed some bugs

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -4,6 +4,6 @@ rm -rf "$HOME/.local/share/nvim"
 mkdir -p "$HOME/.local/bin" "$HOME/.local/share"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 tar -xzf nvim-linux-x86_64.tar.gz
-mv nvim-linux-x86-64.tar.gz nvim
-mv nvim ~/.local/share
+mv nvim-linux-x86_64 "$HOME/.local/share/nvim"
 ln -sf "$HOME/.local/share/nvim/bin/nvim" "$HOME/.local/bin/nvim"
+rm -rf nvim-linux-x86_64.tar.gz
