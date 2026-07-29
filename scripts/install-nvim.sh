254556c8c5037d8424a99fbb154260e2a443c36f
Alex Rull <erasor@erasors-MacBook.local>
2026-07-29 20:28:29 +0200

    script is now arm AND x86 compatible

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 cd "$HOME" || exit 1
+
+ARCH=$(uname -m)
+
+case "$ARCH" in
+x86_64)
+    NVIM_ARCH="x86_64"
+    ;;
+aarch64 | arm64)
+    NVIM_ARCH="arm64"
+    ;;
+*)
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+    ;;
+esac
+
+NVIM_TAR="nvim-linux-$NVIM_ARCH.tar.gz"
+
 rm -rf "$HOME/.local/share/nvim"
 mkdir -p "$HOME/.local/bin" "$HOME/.local/share"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-tar -xzf nvim-linux-x86_64.tar.gz
-mv nvim-linux-x86_64 "$HOME/.local/share/nvim"
+
+curl -LO "https://github.com/neovim/neovim/releases/latest/download/$NVIM_TAR"
+tar -xzf "$NVIM_TAR"
+mv "nvim-linux-$NVIM_ARCH" "$HOME/.local/share/nvim"
 ln -sf "$HOME/.local/share/nvim/bin/nvim" "$HOME/.local/bin/nvim"
-rm -rf nvim-linux-x86_64.tar.gz
+rm -rf "$NVIM_TAR"
+
 cd "$HOME/dotfiles" || exit 1
 bash "$HOME/dotfiles/scripts/update-nvim-config.sh"
